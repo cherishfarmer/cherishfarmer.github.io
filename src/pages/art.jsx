1f1ecdff7ad980870aabdcab4665655025792e3d
Cherish Farmer <cherish.d.farmer@vanderbilt.edu>
2025-12-23 23:26:32 -0600

    added new art

--- src/pages/art.jsx
+++ src/pages/art.jsx
@@ -14,11 +14,19 @@ export const Art = () => {
                      height="250px" width="250px"/>
                 <img className="main-art" src="/media/digital/original/IMG_0171.png"
                      height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/studies/IMG_0495.png"
+                     height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/studies/IMG_0408.png"
+                     height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/studies/IMG_0407.png"
+                     height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/studies/IMG_0406.png"
+                     height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/studies/IMG_0487.png"
+                     height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/studies/IMG_0494.png"
+                     height="250px" width="250px"/>
                 <img className="main-art" src="/media/digital/original/IMG_0179.png"
-                     height="250px" width="250px"/>
-                <img className="main-art" src="/media/digital/assets/coffeepocalypse.png"
-                     height="250px" width="250px"/>
-                <img className="main-art" src="/media/digital/assets/IMG_0128.png"
                      height="250px" width="250px"/>
                 <img className="main-art" src="/media/digital/studies/IMG_0184.png"
                      height="250px" width="250px"/>
@@ -34,9 +42,19 @@ export const Art = () => {
                 <img className="main-art"
                      src="/media/traditional/drawing/IMG_20221114_184320_637.jpg"
                      height="350px" width="250px"/>
+                <img className="main-art" src="/media/digital/assets/coffeepocalypse.png"
+                     height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/assets/IMG_0128.png"
+                     height="250px" width="250px"/>
                 <img className="main-art" src="/media/digital/original/IMG_0199.png"
                      height="250px" width="250px"/>
                 <img className="main-art" src="/media/digital/original/yukari_.jpg"
+                     height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/original/IMG_0331.png"
+                     height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/original/IMG_0488.png"
+                     height="250px" width="250px"/>
+                <img className="main-art" src="/media/digital/original/IMG_0489.png"
                      height="250px" width="250px"/>
             </div>
         </div>
