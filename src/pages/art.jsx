a704e8c9619b769cd72e00305be8bd327a525dfe
Cherish Farmer <Cheri@MacBook-Air-183.local>
2026-04-30 11:03:26 -0500

    added new artwork

--- src/pages/art.jsx
+++ src/pages/art.jsx
@@ -12,31 +12,50 @@ export const Art = () => {
                 <img className="main-art art-square" src="/media/digital/original/candice.png"/>
                 <img className="main-art art-square" src="/media/digital/original/cloudcar.jpg"/>
                 <img className="main-art art-square" src="/media/digital/original/IMG_0171.png"/>
+                <img className="main-art art-square" src="/media/digital/original/IMG_0179.png"/>
+                <img className="main-art art-square" src="/media/digital/original/IMG_0199.png"/>
+                <img className="main-art art-square" src="/media/digital/original/yukari_.jpg"/>
+                <img className="main-art art-square" src="/media/digital/original/IMG_0331.png"/>
+                <img className="main-art art-square" src="/media/digital/original/IMG_0488.png"/>
+                <img className="main-art art-square" src="/media/digital/original/IMG_0489.png"/>
+                <img className="main-art art-square" src="/media/digital/original/IMG_0505.png"/>
                 <img className="main-art art-square" src="/media/digital/studies/IMG_0495.png"/>
                 <img className="main-art art-square" src="/media/digital/studies/IMG_0408.png"/>
                 <img className="main-art art-square" src="/media/digital/studies/IMG_0407.png"/>
                 <img className="main-art art-square" src="/media/digital/studies/IMG_0406.png"/>
                 <img className="main-art art-square" src="/media/digital/studies/IMG_0487.png"/>
                 <img className="main-art art-square" src="/media/digital/studies/IMG_0494.png"/>
-                <img className="main-art art-square" src="/media/digital/original/IMG_0179.png"/>
                 <img className="main-art art-square" src="/media/digital/studies/IMG_0184.png"/>
                 <img className="main-art art-square" src="/media/digital/studies/IMG_0183.png"/>
+                <img className="main-art art-square" src="/media/digital/assets/coffeepocalypse.png"/>
+                <img className="main-art art-square" src="/media/digital/assets/IMG_0128.png"/>
+                <img className="main-art art-square" src="/media/digital/assets/cool_bear.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/dustdevil.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/golem.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/iceball.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/oil_slime.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/penguinguy.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/sun.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/warthog.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/yarnball.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/cactus.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/plasticky.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/elephant.gif"/>
+                <img className="main-art art-square" src="/media/digital/assets/bigsquarebutton.webp"/>
+                <img className="main-art art-square" src="/media/digital/assets/Frame_1321317458.webp"/>
+                <img className="main-art art-square" src="/media/digital/assets/gameovermenu.webp"/>
+                <img className="main-art art-rect" src="/media/digital/original/IMG_0504.png"/>
+                <img className="main-art art-rect" src="/media/digital/assets/cheetah.gif"/>
+                <img className="main-art art-rect" src="/media/digital/assets/fox_fort.gif"/>
+                <img className="main-art art-rect" src="/media/digital/assets/tower.gif"/>
                 <img className="main-art art-rect" src="/media/traditional/drawing/IMG_7222.jpg"/>
                 <img className="main-art art-rect" src="/media/traditional/drawing/IMG_7888.jpg"/>
                 <img className="main-art art-rect"
                      src="/media/traditional/drawing/IMG_20220914_144826828.jpg"/>
                 <img className="main-art art-rect"
                      src="/media/traditional/drawing/IMG_20221114_184320_637.jpg"/>
-                <img className="main-art art-square" src="/media/digital/assets/coffeepocalypse.png"/>
-                <img className="main-art art-square" src="/media/digital/assets/IMG_0128.png"/>
-                <img className="main-art art-square" src="/media/digital/original/IMG_0199.png"/>
-                <img className="main-art art-square" src="/media/digital/original/yukari_.jpg"/>
-                <img className="main-art art-square" src="/media/digital/original/IMG_0331.png"/>
-                <img className="main-art art-square" src="/media/digital/original/IMG_0488.png"/>
-                <img className="main-art art-square" src="/media/digital/original/IMG_0489.png"/>
             </div>
         </div>
         </div>
     )
 }
-
